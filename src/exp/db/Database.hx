f2e555678d0ea00befdda1a14109f9933eb7515c
Kevin Leung <kevinresol@gmail.com>
2020-06-28 21:44:52 +0800

    Fix app compilation

--- src/exp/db/Database.hx
+++ src/exp/db/Database.hx
@@ -3,11 +3,16 @@ package exp.db;
 import exp.db.Table;
 import tink.pure.List;
 
+typedef Database = {
+	final tables:List<Table>;
+	final types:List<CustomType>;
+}
 
 typedef DatabaseSchema = {
 	final tables:List<TableSchema>;
 	final types:List<CustomType>;
 }
+
 typedef DatabaseContent = {
 	final tables:List<TableContent>;
 }
--- src/exp/db/Table.hx
+++ src/exp/db/Table.hx
@@ -2,11 +2,16 @@ package exp.db;
 
 import tink.pure.List;
 
-typedef TableSchema = {
+typedef Table = TableSchema & TableContent;
+typedef TableSchema = Name & Schema;
+typedef TableContent = Name & Content;
+
+private typedef Name = {
 	final name:String;
+}
+private typedef Schema = {
 	final columns:List<Column>;
 }
-typedef TableContent = {
-	final name:String;
+private typedef Content = {
 	final rows:List<Row>;
 }
