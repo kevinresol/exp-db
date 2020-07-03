9f1099b00b7ac0b5ae6d507b3067801af0a6839e
Kevin Leung <kevinresol@gmail.com>
2020-07-03 17:18:18 +0800

    Support save hotkey

--- src/exp/db/app/Electron.hx
+++ src/exp/db/app/Electron.hx
@@ -1,15 +1,17 @@
 package exp.db.app;
 
+import electron.main.MenuItem;
 import js.Node;
 import js.Node.__dirname;
 import electron.main.App;
+import electron.main.Menu;
 import electron.main.BrowserWindow;
 
 class Electron {
 
 	static function main() {
 
-		electron.main.App.on( ready, function(e) {
+		App.on(ready, function(e) {
 
 			var win = new BrowserWindow( {
 				width: 1440, height: 900,
@@ -17,18 +19,36 @@ class Electron {
 					nodeIntegration: true
 				}
 			} );
-			win.on( closed, function() {
+			win.on(closed, function() {
 				win = null;
 			});
 			win.loadFile( 'app.html' );
 			//win.webContents.openDevTools();
 
-			// var tray = new electron.main.Tray( '${__dirname}/icon-192.png' );
+			// var tray = new Tray( '${__dirname}/icon-192.png' );
+			
+			
+			
+			var menu = Menu.getApplicationMenu();
+			var items:Array<MenuItem> = (cast menu).items;
+			
+			for(item in items) {
+				if(item.label == 'File') {
+					item.submenu.insert(0, new MenuItem({
+						label: 'Save',
+						accelerator: 'CmdOrCtrl+S',
+						click: () -> win.webContents.send('command', 'save'),
+					}));
+				}
+			}
+			
+			Menu.setApplicationMenu(menu);
 		});
 
-		electron.main.App.on( window_all_closed, function(e) {
-			if( Node.process.platform != 'darwin' ) electron.main.App.quit();
+		App.on(window_all_closed, function(e) {
+			if( Node.process.platform != 'darwin' ) App.quit();
 		});
+		
 	}
 
 }
--- src/exp/db/app/ui/AppContainer.hx
+++ src/exp/db/app/ui/AppContainer.hx
@@ -4,6 +4,8 @@ import mui.core.*;
 import exp.db.app.data.AppModel;
 import exp.db.app.ui.component.*;
 import exp.db.app.ui.view.*;
+import why.toast.MaterialUiSnackbar;
+import electron.renderer.IpcRenderer;
 
 class AppContainer extends View {
 	@:attr var app:AppModel;
@@ -12,12 +14,29 @@ class AppContainer extends View {
 		<>
 			<CssBaseline />
 			<if ${app.database != null}>
-				<DatabaseView database=${app.database} onSave=${app.saveDatabase}/>
+				<DatabaseView database=${app.database} onSave=${save}/>
 			<else>
 				<Button variant=${Contained} color=${Primary} onClick=${app.newDatabase}>New</Button>
 				<Button variant=${Contained} color=${Primary} onClick=${app.openDatabase}>Load</Button>
 			</if>
+			<MaterialUiSnackbar ref=${v -> why.Toast.inst = v} renderChildren=${options -> options.title}/>
 		</>
 	';
+	
+	override function viewDidMount() {
+		untilUnmounted({
+			IpcRenderer.on('command', function onCommand(event, data) {
+				if(data == 'save') {
+					if(app.database != null) save();
+				}
+			});
+			IpcRenderer.removeListener.bind('command', onCommand);
+		});
+		
+	}
+	
+	function save() {
+		app.saveDatabase().handle(why.Toast.inst.outcome.bind(_, 'Database Saved'));
+	}
 }
 
