09ac0c1355fb9a5ca2c3c043685210377b2ae4e7
Kevin Leung <kevinresol@gmail.com>
2020-07-04 16:36:35 +0800

    Add proper testing

---
+++ tests/DatabaseTest.hx
@@ -0,0 +1,35 @@
+package;
+
+import foo.Database;
+import foo.tables.Tables;
+import foo.types.Types;
+
+using tink.CoreApi;
+
+@:asserts
+class DatabaseTest {
+	var db:Database;
+	
+	public function new() {}
+
+	@:setup
+	public function setup() {
+		return 
+			switch Database.parse(sys.io.File.getContent('tests/content.json')) {
+				case Success(db):
+					this.db = db;
+					Promise.NOISE;
+				case Failure(e):
+					Promise.reject(e);
+			}
+	}
+	
+	public function run() {
+		var event = db.events['id_0'];
+		asserts.assert(event != null, 'Get entry by id');
+		asserts.assert(event.e.match(Grow(1)));
+		asserts.assert(event.enm == Aa);
+		asserts.assert(event.r.get().id == 'id_0');
+		return asserts.done();
+	}
+}
--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -5,25 +5,10 @@ import tink.testrunner.*;
 
 class RunTests {
 	static function main() {
-		switch foo.Database.parse(sys.io.File.getContent('tests/content.json')) {
-			case Success(db):
-				var event = db.events['id_0'];
-				switch event.e {
-					case Grow(v): trace('grow $v');
-					case Combined(e1, e2): trace('combined, $e1, $e2');
-				}
-				switch event.enm {
-					case Aa: trace('a');
-					case Bb: trace('b');
-					case Cc: trace('c');
-				}
-				trace(event.r.get());
-			case Failure(e):
-				trace(e);
-		}
 		
 		Runner.run(TestBatch.make([
 			new ParserTest(),
+			new DatabaseTest(),
 		])).handle(Runner.exit);
 	}
 }
